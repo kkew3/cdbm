5df0cd5165b68dbefb2c469af6a309b9d10ee38e
kkew3 <kps6326@hotmail.com>
2022-02-18 12:10:04 +0800

    List in color only if stdout is attached to a terminal.

--- list_cdbm.py
+++ list_cdbm.py
@@ -1,4 +1,5 @@
 import sys
+import shutil
 
 if sys.platform == 'win32':
     import colorama
@@ -12,16 +13,21 @@ else:
         colorama.init()
 
 cdbm_file = sys.argv[1]
-with open(cdbm_file) as infile:
-    key_width = 0
-    for line in infile:
-        if not line.startswith('#'):
-            key_width = max(key_width, len(line.split(maxsplit=1)[0]))
-    infile.seek(0)
-    for line in infile:
-        if line.startswith('#'):
-            sys.stdout.write('\033[90m{}\033[0m'.format(line))
-        else:
-            tokens = line.split(maxsplit=1)
-            sys.stdout.write('\033[1;31m{}\033[0m {}'.format(
-                tokens[0].ljust(key_width), ''.join(tokens[1:]) or '\n'))
+
+if sys.stdout.isatty():
+    with open(cdbm_file) as infile:
+        key_width = 0
+        for line in infile:
+            if not line.startswith('#'):
+                key_width = max(key_width, len(line.split(maxsplit=1)[0]))
+        infile.seek(0)
+        for line in infile:
+            if line.startswith('#'):
+                sys.stdout.write('\033[90m{}\033[0m'.format(line))
+            else:
+                tokens = line.split(maxsplit=1)
+                sys.stdout.write('\033[1;31m{}\033[0m {}'.format(
+                    tokens[0].ljust(key_width), ''.join(tokens[1:]) or '\n'))
+else:
+    with open(cdbm_file) as infile:
+        shutil.copyfileobj(infile, sys.stdout)
