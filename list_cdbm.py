4b4f8846c989ef7af40dd664f2fb03fab7da2bbc
kkew3 <kps6326@hotmail.com>
2023-01-17 15:16:49 +0800

    Made ascii color code formatting more human readable.

--- list_cdbm.py
+++ list_cdbm.py
@@ -11,6 +11,9 @@ if sys.platform == 'win32':
 cdbm_file = sys.argv[1]
 
 if sys.stdout.isatty():
+    GRAY = '\033[90m'
+    BRIGHT_RED = '\033[1;31m'
+    RST = '\033[0m'
     with open(cdbm_file, encoding='utf-8') as infile:
         key_width = 0
         for line in infile:
@@ -19,11 +22,12 @@ if sys.stdout.isatty():
         infile.seek(0)
         for line in infile:
             if line.startswith('#'):
-                sys.stdout.write('\033[90m{}\033[0m'.format(line))
+                sys.stdout.write('{}{}{}'.format(GRAY, line, RST))
             else:
                 tokens = line.split(maxsplit=1)
-                sys.stdout.write('\033[1;31m{}\033[0m {}'.format(
-                    tokens[0].ljust(key_width), ''.join(tokens[1:]) or '\n'))
+                sys.stdout.write('{}{}{} {}'.format(
+                    BRIGHT_RED, tokens[0].ljust(key_width), RST,
+                    ''.join(tokens[1:]) or '\n'))
 else:
     with open(cdbm_file, encoding='utf-8') as infile:
         shutil.copyfileobj(infile, sys.stdout)
