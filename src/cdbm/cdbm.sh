f158520e622e2a41f6f76430ea7ec535f88647ed
kkew3 <kps6326@hotmail.com>
2024-03-01 19:59:02 +0800

    Add cdbm (shell) entry function

---
+++ src/cdbm/cdbm.sh
@@ -0,0 +1,33 @@
+cdbm() {
+    local fun=
+    local q=
+    case "$1" in
+        -h)
+            fun=help
+            ;;
+        -l)
+            fun=list-bm
+            ;;
+        -c)
+            fun=list-ct
+            ;;
+        -e)
+            fun=edit-bm
+            ;;
+        *)
+            fun=query
+            q="$*"
+            ;;
+    esac
+
+    local ret_path=
+    case $fun in
+        query)
+            ret_path="$(cdbm query "$q")"
+            cd "$ret_path"
+            ;;
+        *)
+            cdbm $fun
+            ;;
+    esac
+}
